04499a1d327a46ac997502f841428594a6128d78
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-13 02:10:06 -0300

    Impede que mais de um item seja selecionado ao mesmo tempo

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const addTaskButton = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 const list = document.getElementById('lista-tarefas');
+const clicked = document.getElementsByClassName('clicked');
 
 addTaskButton.addEventListener('click', () => {
   const inputValue = input.value;
@@ -13,7 +14,10 @@ addTaskButton.addEventListener('click', () => {
       const target = event.target;
       if (target.className.includes('clicked')) {
         target.classList.remove('clicked');
+      } else if (clicked.length === 0) {
+        target.classList.add('clicked');
       } else {
+        clicked[0].classList.remove('clicked');
         target.classList.add('clicked');
       }
     })
